5655eacc923201b37d1bea3ab4c2d86c542c2420
yangshanghui <yangshanghui@instwall.com>
2019-02-18 16:50:35 +0800

    change

--- knows/app2/models.py
+++ knows/app2/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+TYPE = (
+    (1, u"老师"),
+    (2, u"非老师"),
+)
+
 class Teacher(models.Model):
     card_id = models.IntegerField(primary_key=True)
+    type = models.SmallIntegerField(default=1, choices=TYPE, db_index=True)
     name = models.CharField(max_length=30)
 
 class Major(models.Model):
