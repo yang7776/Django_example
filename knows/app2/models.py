00776081189ebbdfad2ace39a81a18320df8ee7d
yangshanghui <yangshanghui@instwall.com>
2019-02-20 14:55:58 +0800

    change

--- knows/app2/models.py
+++ knows/app2/models.py
@@ -20,3 +20,23 @@ class Student(models.Model):
     name = models.CharField(max_length=30)
     classroom = models.CharField(max_length=100)
     major_id = models.ManyToManyField(Major)
+    
+class Test_item_Manger(models.Manager):
+    def judge_id(self,test_id):
+        if test_id>7:
+            return self.get(test_id=test_id).test_name
+        else:
+            return "test_id小于7"
+    
+class Test_item(models.Model):
+    test_id = models.IntegerField(primary_key=True)
+    test_name = models.CharField(max_length=30)
+    test_time = models.DateTimeField(auto_now_add=True)
+    create_time = models.DateTimeField(auto_now=True,db_index=True)
+    objects = Test_item_Manger()    # 相当于对模型中创造一个字段，但是这个字段对应的是一个“方法”
+    class Meta:         # 更多“Meta”的属性，可看博客：https://blog.csdn.net/weixin_40933787/article/details/79425581
+        app_label = "app2"     # 指定这个模型是在哪个工程下，一般用于不在对应app下创建的模型，然后指定这个模型属于谁
+        db_table = "test_of_item"   # 指定这个模型的表名
+        ordering = ["test_id"]      # 指定这个模型的排序根据哪个字段
+        
+        
--- knows/knows/settings.py
+++ knows/knows/settings.py
@@ -93,14 +93,25 @@ WSGI_APPLICATION = 'knows.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'y_test',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'y_test',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'PORT':'5432',
     }
 }
 
