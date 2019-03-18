abe1f3111f863ccfe3dc8e15ae61b4ec767973e1
yangshanghui <yangshanghui@instwall.com>
2019-03-18 09:35:01 +0800

    change

--- knows/app1/forms.py
+++ knows/app1/forms.py
@@ -6,3 +6,14 @@ class UserInfo(forms.Form):
     username = forms.CharField(label="用户名",required=True,max_length=3,help_text="此项必填",
                                error_messages={'max_length':"最多三个字符"})
     iphone = forms.CharField(label="手机号",required=False)
+
+
+    # cinema_ip = forms.CharField(required=True, help_text=u"请正确填写影院IP", max_length=15, min_length=7, regex=ip_regex,
+    #                             error_messages={
+    #                                 "required": u"此项必填", "max_length": u'请输入不超过{0}字符'.format(15),
+    #                                 "min_length": u'请输入不超过{0}字符'.format(7), 'invalid': u'请正确填写影院IP'
+    #                             })
+    # repair_per_tel = forms.RegexField(required=True, max_length=11, min_length=11, help_text=u"请正确填写手机号码",
+    #                                   regex=iphone_regex, error_messages={
+    #         "required": u"此项必填", "max_length": u'请输入不超过11字符', "min_length": u'请输入不超过11字符', 'invalid': u'请正确填写手机号码'
+    #     })
