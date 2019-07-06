252c3b62352e1eb560e33495cefb4118dd2e0dbb
yang <ysh_7776@163.com>
2019-07-06 16:44:06 +0800

    update

--- knows/knows/urls.py
+++ knows/knows/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include("app1.urls")),
     path('app2/', include("app2.urls")),
-    path('restful/', include("restful.urls")),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
