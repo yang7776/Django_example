1fb64e181548fdf659f778cc69518330976f88dd
majiankai <majiankai@instwall.com>
2019-07-08 14:51:54 +0800

    ysh

---
+++ py_frame/asyncio_methed.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# writer        Yang   
+# create_time   2019/6/27 14:22
+# file_name     asyncio.py
+
+# 博客：https://www.cnblogs.com/shenh/p/9090586.html
+import time
+import asyncio
+
+# 定义异步函数
+# async def hello():
+#     asyncio.sleep(1)
+#     print('Hello World:%s' % time.time())
+#
+# def run():
+#     for i in range(5):
+#         loop.run_until_complete(hello())
+#
+# loop = asyncio.get_event_loop()
+# if __name__ =='__main__':
+#     run()
+
+a = '6pl_pl_6'
+print(a.find('pl'))
