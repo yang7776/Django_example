98444c1b96270c5367344872fde93b705e928885
majiankai <majiankai@instwall.com>
2019-07-31 10:07:19 +0800

    ysh

--- py_frame/os.py
+++ py_frame/os.py
@@ -21,12 +21,19 @@ path4 = os.path.join(path3,'os.txt')
 如果以 r+、w、w+、a、a+ 模式打开文件，则都可以写入。需要指出的是，当以 r+、w、w+ 模式打开文件时，文件指针位于文件开头处；当以 a、a+ 模式打开文件时，文件指针位于文件结尾处。
 """
 # 打开文件操作，“with open”和“os.open”的区别就是，with是自动关闭文件的。
-with open(path4,'r') as f:
-	print(f.read())
+try:
+	with open(path4,'r',encoding='utf-8') as f:
+		print(f.read())
+except FileNotFoundError:
+	print('无法打开指定的文件!')
+except LookupError:
+	print('指定了未知的编码!')
+except UnicodeDecodeError:
+	print('读取文件时解码错误!')
 # 写入文件操作
-with open(path4,'a') as f:
-	f.write('this is a test/')
-	
+# with open(path4,'a') as f:
+# 	f.write('this is a test/')
+
 # print(path1)
 # print(path2)
 # print(path3)
